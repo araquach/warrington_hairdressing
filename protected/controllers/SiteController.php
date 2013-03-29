4e2fb8f12bf64ba44872ee777d8daa0ce237922f
Araquach <adam@contra-design.com>
2013-03-29 16:38:51 +0000

    Redirected to correct page and minor form styling changes

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -92,7 +92,6 @@ class SiteController extends Controller
 			$model->attributes=$_POST['LoginForm'];
 			// validate user input and redirect to the previous page if valid
 			if($model->validate() && $model->login())
-				Yii::app()->user->setReturnUrl(array('index'));
 				$this->redirect(Yii::app()->user->returnUrl);
 		}
 		// display the login form
