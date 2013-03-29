25ab4f96e936b0b869ca4c6f86db65b433ddd021
Araquach <adam@contra-design.com>
2013-03-29 13:24:55 +0000

    Revert "Revert "Changed the query""
    This reverts commit b6db6b8cb8044b35cbebd7d7ac87b641b872f197.

--- protected/models/Holiday.php
+++ protected/models/Holiday.php
@@ -161,16 +161,12 @@ class Holiday extends SalonActiveRecord
 	public function totalHoliday()
 	{
 	
-		$criteria = new CDbCriteria();
-		$criteria->with = 'staff';
-		$criteria->condition = 'staff.id=' . Yii::app()->user->id;
-		$criteria->select = 'hours_requested';
-		
-		$total = Holiday::model()->count($criteria);
+		$total = Holiday::model()->with('staff')->findBySql('select sum(`hours_requested`) as `sum` from Holiday', array());
 		
 		return $total;
 		
 	}
+	
 	
 	
 }
