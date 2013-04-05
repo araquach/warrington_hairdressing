bcab356a2107cbd2a4086de6ee9a66d2412c00c7
Araquach <adam@contra-design.com>
2013-04-05 09:00:57 +0100

    Flipped the choices around

--- protected/views/holiday/_approval.php
+++ protected/views/holiday/_approval.php
@@ -16,13 +16,13 @@
 	<div class="row">
 		<?php echo $form->labelEx($model,'approved'); ?>
 		
-		<?php echo '<p class="scale_label">No</p>' ?>
-		<?php echo $form->radioButton($model,'approved', array(
-		'value'=>1,
-		'uncheckValue'=>null));?>
 		<?php echo '<p class="scale_label">Yes</p>' ?>
 		<?php echo $form->radioButton($model,'approved', array(
 		'value'=>2,
+		'uncheckValue'=>null));?>
+		<?php echo '<p class="scale_label">No</p>' ?>
+		<?php echo $form->radioButton($model,'approved', array(
+		'value'=>1,
 		'uncheckValue'=>null));?>
 		
 		<?php echo $form->error($model,'approved'); ?>
--- protected/views/lieu/_approval.php
+++ protected/views/lieu/_approval.php
@@ -10,19 +10,17 @@
 	'id'=>'holiday-form',
 	'enableAjaxValidation'=>false,
 )); ?>
-
-
-		
+	
 	<div class="row group">
 		<?php echo $form->labelEx($model,'approved'); ?>
 		
-		<?php echo '<p class="scale_label">No</p>' ?>
-		<?php echo $form->radioButton($model,'approved', array(
-		'value'=>1,
-		'uncheckValue'=>null));?>
 		<?php echo '<p class="scale_label">Yes</p>' ?>
 		<?php echo $form->radioButton($model,'approved', array(
 		'value'=>2,
+		'uncheckValue'=>null));?>
+		<?php echo '<p class="scale_label">No</p>' ?>
+		<?php echo $form->radioButton($model,'approved', array(
+		'value'=>1,
 		'uncheckValue'=>null));?>
 		
 		<?php echo $form->error($model,'approved'); ?>
