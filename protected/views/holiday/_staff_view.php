e54136eee0ff148f48d899f249bce379ab922237
Araquach <adam@contra-design.com>
2013-04-05 12:15:03 +0100

    traffic light system working

--- protected/views/holiday/_staff_view.php
+++ protected/views/holiday/_staff_view.php
@@ -3,16 +3,17 @@
 /* @var $model Holiday */
 ?>
 
-<div class="view">
+<div class="view <?php if ($data->approved == 1) {
+	echo 'unapproved'; } elseif ($data->approved == 2) {
+		echo 'approved'; } else {
+			echo 'pending';
+		}
+?> " >
 
 	<?php echo CHtml::link(CHtml::encode($data->id), array('staff_view', 'id'=>$data->id)); ?>
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('hours_requested')); ?>:</b>
 	<?php echo CHtml::encode($data->hourConverter()); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('prebooked')); ?>:</b>
-	<?php echo CHtml::encode($data->getPrebookedText()); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('request_date_from')); ?>:</b>
