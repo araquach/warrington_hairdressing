22a4d5049e4369acd2f4ec8e45ce68b36ba1f82d
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-04-07 00:40:28 +0100

    created reamining holiday calculation

--- protected/components/HolidayWidget.php
+++ protected/components/HolidayWidget.php
@@ -5,15 +5,26 @@ class HolidayWidget extends CWidget
 
     public function run()
     {
+    	// total holiday calculation
         $total = Yii::app()->db->createCommand()
         ->select('sum(hours_requested)')
         ->from('holiday')
         ->where('staff_id=' . Yii::app()->user->id)
         ->queryScalar();
         
-        $total =$total/8;
+        $total = $total/8;
         
-        $this->render('holidayWidget', array('total'=>$total));
+        // remaining holiday calculation
+        $entitlement = Yii::app()->db->createCommand()
+        ->select('holiday_entitlement')
+        ->from('staff')
+        ->where('id=' . Yii::app()->user->id)
+        ->queryScalar();
+        
+        $remaining = $entitlement - $total;
+        
+        
+        $this->render('holidayWidget', array('total'=>$total, 'remaining'=>$remaining));
     }
 }
 
--- protected/components/views/holidayWidget.php
+++ protected/components/views/holidayWidget.php
@@ -1,4 +1,4 @@
 <?php echo CHtml::link('<div class="menuBox wide holiday" id="holiday">
 	<h2>Holidays</h2>
-	<p>' . $total . ' booked</p>
+	<p>' . $remaining . ' remaining </p>
 </div>', array('/holiday/staff_holiday')); ?>	
