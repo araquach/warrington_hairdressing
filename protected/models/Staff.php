bdd4a332ba94c88a6b9aa9d71c389daa06cd6758
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-04-21 10:36:14 +0100

    Created full name concat

--- protected/models/Staff.php
+++ protected/models/Staff.php
@@ -146,4 +146,9 @@ class Staff extends SalonActiveRecord
 		));
 	}
 	
+	public function getFullName()
+	{
+		return $this->first_name . ' ' . $this->last_name;
+	}
+	
 }
--- protected/views/lieu/_form.php
+++ protected/views/lieu/_form.php
@@ -18,7 +18,7 @@
 	<div class="row">
 		<?php echo $form->labelEx($model,'staff_id'); ?>
 		<?php echo $form->dropDownList($model, 'staff_id', CHtml::listData(
-		Staff::model()->findAll(), 'id', 'first_name'),
+		Staff::model()->findAll(), 'id', 'fullName'),
 		array('prompt' => 'Select Staff Member')); ?>
 		<?php echo $form->error($model,'staff_id'); ?>
 	</div>
