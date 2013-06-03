11ab342d5b4ec13eab9d2c07d7f1d0cec32d8b9a
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-06-03 14:11:43 +0100

    some changes

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -44,8 +44,8 @@
 		<div id="mainmenu">
 			<?php $this->widget('zii.widgets.CMenu',array(
 				'items'=>array(
-					array('label'=>'Home', 'url'=>array('//home/index')),
-					array('label'=>'Admin', 'url'=>array('//home/admin_index'), 'visible'=>isset(Yii::app()->user->role) && (Yii::app()->user->role==="admin")),
+					array('label'=>'Home', 'url'=>array('/home/index')),
+					array('label'=>'Admin', 'url'=>array('/home/admin_index'), 'visible'=>isset(Yii::app()->user->role) && (Yii::app()->user->role==="admin")),
 					array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 					array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 				),
--- recruit_applicant.sql
+++ recruit_applicant.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 03, 2013 at 12:33 PM
+-- Generation Time: Jun 03, 2013 at 01:11 PM
 -- Server version: 5.1.44
 -- PHP Version: 5.3.1
 
@@ -29,15 +29,24 @@ CREATE TABLE IF NOT EXISTS `recruit_applicant` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `person_id` int(11) NOT NULL,
   `salon_id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `person_id` (`person_id`),
-  KEY `salon_id` (`salon_id`),
-  KEY `role_id` (`role_id`)
+  KEY `salon_id` (`salon_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `recruit_applicant`
 --
 
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `recruit_applicant`
+--
+ALTER TABLE `recruit_applicant`
+  ADD CONSTRAINT `recruit_applicant_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `recruit_applicant_ibfk_2` FOREIGN KEY (`salon_id`) REFERENCES `salon` (`id`) ON DELETE CASCADE;
